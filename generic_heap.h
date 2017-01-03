50d4da87c8423f0648775056c60a1e1b96448f48
GavinHigham <Gavin.Higham@gmail.com>
2017-01-02 18:49:20 -0800

    Fixed outdated header comments

--- generic_heap.h
+++ generic_heap.h
@@ -10,11 +10,11 @@ typedef struct
 //Returns a new HEAP of length "len."
 HEAP GENERIC(new_heap)(GENERIC_TYPE arr[], int len);
 
-//Adds a void * v into the heap ph, percolating it up to the correct level, based on the provided comparison function.
+//Adds an item into the heap ph, percolating it up to the correct level, based on the provided comparison function.
 //Returns 0 if v was successfully added to the heap.
 //Returns -1 if the heap was full and could not be enlarged. (v was not added in this case)
 int GENERIC(heap_add)(HEAP *ph, GENERIC_TYPE v, int (*compare)(GENERIC_TYPE, GENERIC_TYPE));
 
-//Removes a void * from the heap ph, maintaining heapiness.
-//Returns a void *, or NULL if there are no more items in the queue.
+//Removes an item from the heap ph, maintaining heapiness.
+//Asserts if there are no more items in the heap, because laziness.
 GENERIC_TYPE GENERIC(heap_rem)(HEAP *ph, int (*compare)(GENERIC_TYPE, GENERIC_TYPE));
